778a7bc7c20cabbff0f062580581cc602e51b6e5
kristiansota <sotakristi@outlook.com>
2019-10-31 13:08:21 +0100

    product-item component implemented with property binding and work on product-item.css

--- src/app/product/products-list/products-list.component.ts
+++ src/app/product/products-list/products-list.component.ts
@@ -9,10 +9,10 @@ import { Product } from '../product.model';
 export class ProductsListComponent implements OnInit {
 
   products: Product[] = [
-    new Product(1,'Dell Alienware t510',100.00,'Gaming PC With 8gb Graphics Card','https://upload.wikimedia.org/wikipedia/commons/8/88/AVADirect_God_Mode_Extreme_Gaming_PC.png'),
-    new Product(1,'Dell Alienware t510',100.00,'Gaming PC With 8gb Graphics Card','https://upload.wikimedia.org/wikipedia/commons/8/88/AVADirect_God_Mode_Extreme_Gaming_PC.png'),
-    new Product(1,'Dell Alienware t510',100.00,'Gaming PC With 8gb Graphics Card','https://upload.wikimedia.org/wikipedia/commons/8/88/AVADirect_God_Mode_Extreme_Gaming_PC.png'),
-    new Product(1,'Dell Alienware t510',100.00,'Gaming PC With 8gb Graphics Card','https://upload.wikimedia.org/wikipedia/commons/8/88/AVADirect_God_Mode_Extreme_Gaming_PC.png')
+    new Product(1,'Dell Alienware T510',100.00,'Gaming PC With 8gb Graphics Card','https://upload.wikimedia.org/wikipedia/commons/8/88/AVADirect_God_Mode_Extreme_Gaming_PC.png'),
+    new Product(1,'Dell Alienware T510',100.00,'Gaming PC With 8gb Graphics Card','https://upload.wikimedia.org/wikipedia/commons/8/88/AVADirect_God_Mode_Extreme_Gaming_PC.png'),
+    new Product(1,'Dell Alienware T510',100.00,'Gaming PC With 8gb Graphics Card','https://upload.wikimedia.org/wikipedia/commons/8/88/AVADirect_God_Mode_Extreme_Gaming_PC.png'),
+    new Product(1,'Dell Alienware T510',100.00,'Gaming PC With 8gb Graphics Card','https://upload.wikimedia.org/wikipedia/commons/8/88/AVADirect_God_Mode_Extreme_Gaming_PC.png')
   ];
 
   constructor() { }
